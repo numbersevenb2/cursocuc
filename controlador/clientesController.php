fc4c3f6330236e0d5ad175408dacd7ab1e7af2a2
numbersevenb2 <gonzalezpuentejuanalberto3@gmail.com>
2024-01-30 10:40:54 -0600

    tarea xd

---
+++ controlador/clientesController.php
@@ -0,0 +1,11 @@
+<?php
+
+    switch($opc){
+        case 1: // Registrar comentarios de los clientes
+            $nombre = $_POST['txtNombre'];
+            $email  = $_POST['txtEmail'];
+            $tel    = $_POST['txtTelefono'];
+            $coment = $_POST['txtComentarios'];
+    }
+
+?>
